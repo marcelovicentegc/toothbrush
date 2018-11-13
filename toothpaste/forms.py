c21c9e07f5b2b3d0f5d107b1655612518da13064
Marcelo Cardoso <marcelovicentegc@gmail.com>
2018-11-13 09:30:42 -0300

    Added help_text

--- toothpaste/forms.py
+++ toothpaste/forms.py
@@ -6,7 +6,7 @@ from toothpaste.models import DocumentModel
 
 
 class DocumentForm(ModelForm):
-    document = forms.FileField(max_length=255, required=True)
+    document = forms.FileField(max_length=255, required=True, help_text="Files with accents in their names won't upload")
 
     class Meta:
         model = DocumentModel
