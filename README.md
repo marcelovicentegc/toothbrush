e6a61b1518c0eac6f53a9b7b5fddeae03293abd9
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-02-08 11:11:20 -0300

    Added missing files

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ A web-app that extracts the 20 most common words from .pdf, .doc and .txt files.
 
 ## Demo
 <img src="https://github.com/marcelovicentegc/Toothbrush/blob/master/Toothbrush.gif" width="640" height="360" />
+
+## Directions
+1. Clone this repository:
+2. Install dependencies: `cd Toothbrush/project/requirements; pip install -r base.txt` if on Windows, or `cd Toothbrush/project/requirements && pip install -r base.txt` if on Mac/Linux
+3. Run migrations: `cd ../..; python manage.py makemigrations; python manage.py makemigrations toothpaste` if on Windows, or `cd ../.. && python manage.py makemigrations && python manage.py makemigrations toothpaste` if on Mac/Linux
+4. Migrate database: `python manage.py migrate`
+5. Run the application: `python manage.py runserver`
--- toothpaste/tests.py
+++ toothpaste/tests.py
@@ -1,24 +1,44 @@
-from django.test import TestCase
-
-# ResultView
+import os
+from project.settings.base import MEDIA_ROOT
+from django.test import TestCase, Client
 from toothpaste.models import DocumentModel
 from toothpaste.toilet_sink.soap import ToiletSink
 
-ToiletSink = ToiletSink()
-object_list = DocumentModel.objects.all()
-obj = object_list[0] # .txt
-obj1 = object_list[13] # .docx
-obj2 = object_list[14] # .pdf
-document = obj.document.path
-document1 = obj1.document.path
-document2 = obj2.document.path
+path = os.path.join(MEDIA_ROOT, 'documents')
 
-f = ToiletSink.text_extractor(document)
-f1 = ToiletSink.text_extractor(document1)
-f2 = ToiletSink.text_extractor(document2)
+c = Client(enforce_csrf_checks=True)
+c.get('/')
+c.get('/about/')
+with open(path + '/ESPN Case Study.docx', 'rb') as tf:
+    c.post('/', {'document': tf}, follow=True)
+with open(path + '/Renewable Energy Statistics 2017.pdf', 'rb') as tf:
+    c.post('/', {'document': tf}, follow=True)
+with open(path + '/The Fox News Effect - Media Bias and Voting.pdf', 'rb') as tf:
+    c.post('/', {'document': tf}, follow=True)
 
-f = ToiletSink.final_cut(f)
-f1 = ToiletSink.final_cut(f1)
-f2 = ToiletSink.final_cut(f2)
+class ResultTestCase(TestCase):
+    ToiletSink = ToiletSink()
+
+    def setUp(self):
+        DocumentModel.objects.create(document=('documents/ESPN Case Study.docx'))
+        DocumentModel.objects.create(document=('documents/Renewable Energy Statistics 2017.pdf'))
+        DocumentModel.objects.create(document=('documents/The Fox News Effect - Media Bias and Voting.pdf'))
+
+    def test_flush(self):
+        object_list = DocumentModel.objects.all()
+        obj = object_list[0]
+        obj1 = object_list[1]
+        obj2 = object_list[2]
+        document = obj.document.path
+        document1 = obj1.document.path
+        document2 = obj2.document.path
+
+        f = self.ToiletSink.text_extractor(document)
+        f1 = self.ToiletSink.text_extractor(document1)
+        f2 = self.ToiletSink.text_extractor(document2)
+
+        f = self.ToiletSink.final_cut(f)
+        f1 = self.ToiletSink.final_cut(f1)
+        f2 = self.ToiletSink.final_cut(f2)
 
 
